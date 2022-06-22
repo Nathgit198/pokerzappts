43a292ce523fdd32f93b6b3656097c447e400235
nathgit198 <nath.conta14@gmail.com>
2022-06-22 20:55:16 -0300

    Novas funcionalidades: buscar pokemon, paginação e pokedex

--- src/App.js
+++ src/App.js
@@ -1,20 +1,72 @@
-import React, { useEffect } from "react"
-import './App.css';
-import Navbar from './components/Navbar';
+import React, { useEffect, useState } from "react";
+import { getPokemonData, getPokemons, searchPokemon } from "./api";
+import "./App.css";
+import Navbar from "./components/Navbar";
 import Pokedex from "./components/Pokedex";
-import Searchbar from './components/Searchbar';
-
+import Searchbar from "./components/Searchbar";
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const itensPerPage = 25
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage))
+    } catch (error) {
+      console.log("fetchPokemons error:", error);
+    }
+  };
+
   useEffect(() => {
-    console.log("carregou")
-  })
-  
+    fetchPokemons();
+  }, [page]);
+
+  const onSearchHandler = async (pokemon) => {
+    if (!pokemon) {
+      return fetchPokemons();
+    }
+
+    setLoading(true)
+    setNotFound(false)
+    const result = await searchPokemon(pokemon)
+    if (!result) {
+      setNotFound(true)
+    } else {
+      setPokemons([result])
+      setPage(0)
+      setTotalPages(1)
+    }
+    setLoading(false)
+  } 
+
   return (
     <div>
-        <Navbar />
-        <Searchbar />
-        <Pokedex />
+      <Navbar />
+      <Searchbar onSearch={onSearchHandler} />
+      {notFound ? (
+        <div className="notFound-text">Não encontramos esse pokemon, tente digitar novamente</div>
+      ) : 
+      (<Pokedex
+      pokemons={pokemons}
+      loading={loading}
+      page={page}
+        setPage={setPage}
+        totalPages={totalPages}
+        />)
+      }
     </div>
   );
 }
